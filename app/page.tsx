7cf852fc4c058807d113885e03d70e93a13e13fb
harakles <55509530+harakles@users.noreply.github.com>
2023-11-08 14:02:33 +0300

    index image size updated

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image src="/hero-desktop.png" width={1000} height={700} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop and mobile versions"/>
+          <Image src="/hero-desktop.png" width={1000} height={500} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop and mobile versions"/>
           <Image src="/hero-mobile.png" width={560} height={620} className="block md:hidden" alt="Screenshots of the dashboard project showing mobile versions"/>
         </div>
       </div>
