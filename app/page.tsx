c928d8293196a00fb2d4d6940e48f606cd893c02
harakles <55509530+harakles@users.noreply.github.com>
2023-11-08 14:26:20 +0300

    index image bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-30">
         <AcmeLogo /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -30,8 +30,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image src="/hero-desktop.png" width={1000} height={500} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop and mobile versions"/>
+          <Image src="/hero-desktop.png" width={1000} height={700} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop and mobile versions"/>
           <Image src="/hero-mobile.png" width={560} height={620} className="block md:hidden" alt="Screenshots of the dashboard project showing mobile versions"/>
         </div>
       </div>
